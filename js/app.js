004314f650902d61bc0d8817ee253eb63985723f
MStumpp <mstumpp@gmail.com>
2012-07-14 15:30:44 +0200

    changed code formatting

--- js/app.js
+++ js/app.js
@@ -14,31 +14,31 @@ if (Meteor.is_client) {
 
     // Returns an event_map key for attaching "ok/cancel" events to
     // a text input (given by selector)
-  	var okcancel_events = function (selector) {
-  		return 'keyup '+selector+', keydown '+selector+', focusout '+selector;
+	var okcancel_events = function (selector) {
+		return 'keyup '+selector+', keydown '+selector+', focusout '+selector;
 	};
 
 	// Creates an event handler for interpreting "escape", "return", and "blur"
 	// on a text field and calling "ok" or "cancel" callbacks.
 	var make_okcancel_handler = function (options) {
-  		var ok = options.ok || function () {};
-  		var cancel = options.cancel || function () {};
+		var ok = options.ok || function () {};
+		var cancel = options.cancel || function () {};
 
-  		return function (evt) {
-    		if (evt.type === 'keydown' && evt.which === 27) {
-     			// escape = cancel
-      			cancel.call(this, evt);
+		return function (evt) {
+			if (evt.type === 'keydown' && evt.which === 27) {
+				// escape = cancel
+				cancel.call(this, evt);
 
-    		} else if (evt.type === 'keyup' && evt.which === 13 || 
-    			evt.type === 'focusout') {
-      			// blur/return/enter = ok/submit if non-empty
-      			var value = String(evt.target.value || '');
-      			if (value)
-        			ok.call(this, value, evt);
-      			else
-        			cancel.call(this, evt);
-    		}
-  		};
+			} else if (evt.type === 'keyup' && evt.which === 13 || 
+				evt.type === 'focusout') {
+				// blur/return/enter = ok/submit if non-empty
+				var value = String(evt.target.value || '');
+				if (value)
+					ok.call(this, value, evt);
+				else
+					cancel.call(this, evt);
+			}
+		};
 	};
 
     Template.todoapp.has_todos = function() {
